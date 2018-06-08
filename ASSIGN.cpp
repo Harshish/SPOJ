6cc29dc37c7295b572e09c45a6229ec7b66a04dd
Harshish Yadav <harshishyadav@gmail.com>
2018-06-08 18:26:06 +0530

    June 18

--- ASSIGN.cpp
+++ ASSIGN.cpp
@@ -1,47 +1,49 @@
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-	int tc;
-	cin >> tc;
-	while(tc--)
-	{
-		int N;
-		cin >> N;
-		int like[N][N];
-		for(int i=0;i<N;++i)
-		    for(int j=0;j<N;++j)
-		        cin >> like[i][j];
-	
-		int NMAX = (1<<N);
-		int dp[NMAX];
-		for(int i=0;i<NMAX;++i) 
-		    dp[i] = 0;
-		
-		dp[NMAX-1] = 1;
-		for(int b=NMAX-1;b>=0; --b) 
-		{
-		    int n = b;
-		    int k = 0;
-		    while(n)
-		    {
-		        k += (n&1);
-		        n /= 2;
-		    }
-		    if(k==N)
-		    	continue;
-		    for(int i=0;i<N;++i)
-		    {
-		        if(like[k][i] && !(b & (1<<i)))
-		        {
-		            dp[b] += dp[b | (1<<i)];
+int allmask;
 
-		        }
-		    }
-		}
-		cout << dp[0]  << endl;
-	}
-	return 0;
-}                                            
+long long assign(int **mat,long long *dp,int mask,int n)
+{
+    int index = __builtin_popcount(mask);
+    if(index==n && mask==allmask)
+        return 1;
+    if(dp[mask]!=-1)
+        return dp[mask];
+    long long cnt = 0;
+    for(int j = 0; j < n; j++)
+    {
+        if(!mat[index][j] || (mask & (1<<j)))
+            continue;
+        cnt += assign(mat,dp,mask | (1<<j),n);
+    }
+    dp[mask] = cnt;
+    return dp[mask];
+}
+
+
+int main(int argc, char const *argv[])
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int N = 1<<21;
+    long long *dp = new long long[N];
+    int tc;
+    cin >> tc;   
+    while(tc--)
+    {
+        int n;
+        cin >> n;
+        int **mat = new int*[n];
+        for(int i = 0; i < n; i++)
+        {
+            mat[i] = new int[n];
+            for(int j = 0; j < n; j++)
+                cin >> mat[i][j];
+        }
+        memset(dp,-1,N*sizeof(dp[0]));
+        allmask = pow(2,n) - 1;
+        cout << assign(mat,dp,0,n) << "\n";
+    }
+    return 0;
+}
